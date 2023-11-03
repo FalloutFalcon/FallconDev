86650c316d6c3c3df75a38cbc1961718ce3e8427
FalloutFalcon <falloutfalconplays@gmail.com>
2023-11-03 18:09:31 -0500

    Remove unnecessary files and dependencies

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="box">
+        <h3>Welcome to my <strong>blog</strong></h3>
+        <p>This is my little dev website. I'll be posting about my projects and other stuff here.</p>
+      </div>
+      <div className="box">
+        <h3>About Me</h3>
+        <p>I enjoy Team Fortress 2!</p>
+      </div>
+      <div className="box">
+        <h3>Projects</h3>
+        <p>Im currently working on a archive website for my school!</p>
+        <p>I also contribute to the Shiptest fork of ss13!</p>
+        <p>Robotics!</p>
+      </div>
+      <div className="box">
+        <h3>Info</h3>
+        <a href="https://github.com/FalloutFalcon">Github</a>
+        <a href="https://steamcommunity.com/profiles/76561198289230657/">Steam</a>
+        <a href="https://open.spotify.com/user/31ymvtm5x7sdv3375a4bizgr276a">Spotify</a>
+        <img src="moth_hell.gif" className="moth" />
+        <a href="based.png"> <img src="fallcon.png" alt="Gay" className="alstolfo" /> </a>
+      </div>
+    </div>
+  )
+}
