8cd3bf58e5388a067789a45e1fa2e7b595c8b2a8
fallcon <falloutfalconplays@gmail.com>
2023-12-14 12:00:06 -0600

    search indexing i hope

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="R_ySoIu2oKs_b31JCv7joZE6bWwksD8XIT2QkGGpBDU"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
