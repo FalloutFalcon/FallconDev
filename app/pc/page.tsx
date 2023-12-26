06d2e0433c023c2fe988583358c8037acbf7e737
FalloutFalcon <falloutfalconplays@gmail.com>
2023-12-25 18:47:29 -0600

    pc

---
+++ app/pc/page.tsx
@@ -0,0 +1,142 @@
+export default function Page() {
+  return (
+    <div>
+      <a href="https://pcpartpicker.com/list/JwfRn6">PCPartPicker Part List</a>
+      <table className="pcpp-part-list">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Item</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="pcpp-part-list-type">CPU</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/VNkWGX/intel-core-i5-13400f-25-ghz-10-core-processor-bx8071513400f">
+                Intel Core i5-13400F 2.5 GHz 10-Core Processor
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/VNkWGX/intel-core-i5-13400f-25-ghz-10-core-processor-bx8071513400f">
+                $191.99 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">CPU Cooler</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/WKWzK8/deepcool-ag400-bk-argb-7589-cfm-cpu-cooler-r-ag400-bkanmc-g-2">
+                Deepcool AG400 BK ARGB 75.89 CFM CPU Cooler
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/WKWzK8/deepcool-ag400-bk-argb-7589-cfm-cpu-cooler-r-ag400-bkanmc-g-2">
+                $29.99 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Motherboard</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/j2WJ7P/asrock-b760m-hdvm2-micro-atx-lga1700-motherboard-b760m-hdvm2">
+                ASRock B760M-HDV/M.2 Micro ATX LGA1700 Motherboard
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/j2WJ7P/asrock-b760m-hdvm2-micro-atx-lga1700-motherboard-b760m-hdvm2">
+                $119.99 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Memory</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/3L9wrH/crucial-pro-32-gb-2-x-16-gb-ddr5-5600-cl46-memory-cp2k16g56c46u5">
+                Crucial Pro 32 GB (2 x 16 GB) DDR5-5600 CL46 Memory
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/3L9wrH/crucial-pro-32-gb-2-x-16-gb-ddr5-5600-cl46-memory-cp2k16g56c46u5">
+                $84.99 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Storage</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/VZWzK8/crucial-p5-plus-2-tb-m2-2280-pcie-40-x4-nvme-solid-state-drive-ct2000p5pssd8">
+                Crucial P5 Plus 2 TB M.2-2280 PCIe 4.0 X4 NVME Solid State Drive
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/VZWzK8/crucial-p5-plus-2-tb-m2-2280-pcie-40-x4-nvme-solid-state-drive-ct2000p5pssd8">
+                $136.80 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Video Card</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/X7CZxr/asus-dual-oc-geforce-rtx-4060-8-gb-video-card-dual-rtx4060-o8g">
+                Asus DUAL OC GeForce RTX 4060 8 GB Video Card
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/X7CZxr/asus-dual-oc-geforce-rtx-4060-8-gb-video-card-dual-rtx4060-o8g">
+                $299.99 @ Newegg
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Case</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/36gFf7/deepcool-matrexx-40-3fs-microatx-mid-tower-case-dp-matx-matrexx40-3fs">
+                Deepcool MATREXX 40 3FS MicroATX Mini Tower Case
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/36gFf7/deepcool-matrexx-40-3fs-microatx-mid-tower-case-dp-matx-matrexx40-3fs">
+                $64.99 @ B&H
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td className="pcpp-part-list-type">Power Supply</td>
+            <td className="pcpp-part-list-item">
+              <a href="https://pcpartpicker.com/product/b8Jkcf/thermaltake-smart-700w-80-certified-semi-modular-atx-power-supply-ps-spd-0700npcwus-w">
+                Thermaltake Smart 700 W 80+ Certified ATX Power Supply
+              </a>
+            </td>
+            <td className="pcpp-part-list-price">
+              <a href="https://pcpartpicker.com/product/b8Jkcf/thermaltake-smart-700w-80-certified-semi-modular-atx-power-supply-ps-spd-0700npcwus-w">
+                $53.99 @ Amazon
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td className="pcpp-part-list-price-note">
+              Prices include shipping, taxes, rebates, and discounts
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td className="pcpp-part-list-total">Total</td>
+            <td className="pcpp-part-list-total-price">$982.73</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td className="pcpp-part-list-price-note">
+              Generated by <a href="https://pcpartpicker.com">PCPartPicker</a>{" "}
+              2023-12-25 19:26 EST-0500
+            </td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
